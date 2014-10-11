35eb693714f5e4886b90a3e2d9e13274b0a611ba
Den Corbie <corbie.den@mail.ru>
2014-10-11 12:44:27 +0400

    Доработан алгоритм проверки прямоугольником 3х2

--- 2014.10.07 Yandex - spoiled parquet/src/ru/my1/corbie/Main.java
+++ 2014.10.07 Yandex - spoiled parquet/src/ru/my1/corbie/Main.java
@@ -63,37 +63,91 @@ public class Main
         		*/
         		for(short i=0; i < parq.length; i++)
 				{
-					for(short j=0; j < parq[i].length; j++)
-						if (parq[i][j]==1)
-						    if (parq[i][j+1]==1)
-							if (parq[i+1][j]==1)
-							    if (parq[i][j+2]==1)
-								{
-								    parq[i][j]=0;
-								    parq[i][j+1]=0;
-								    parq[i+1][j]=0;
-								    parq[i][j+2]=0;
-								    costCounter += 2*costDouble;
-								}
-							    else {
-								parq[i][j]=0;
-								parq[i][j+1]=0;
-								costCounter += costDouble;
-							    }
-							else {
-							    parq[i][j]=0;
-							    parq[i][j+1]=0;
-							    costCounter += costDouble;
-							}
-						    else if (parq[i+1][j]==1)
-							{
-							    parq[i][j]=0;
-							    parq[i+1][j]=0;
-							    costCounter += costDouble;
-							}
-				}  
+				    for(short j=0; j < parq[i].length; j++)
+					//Проверка возможности использования метода с прямоугольником 3х2
+					if (!(i==parq.length))
+					    {
+					    if (j<parq[i].length-1)
+						{
+    						//Начало цикла основной проверки
+    						if (parq[i][j]==1)
+    						    if (parq[i][j+1]==1)
+    							if (parq[i+1][j]==1)
+    							    if (parq[i][j+2]==1)
+    								{
+    								    parq[i][j]=0;
+    								    parq[i][j+1]=0;
+    								    parq[i+1][j]=0;
+    								    parq[i][j+2]=0;
+    								    costCounter += 2*costDouble;
+    								}
+    							    else {
+    								parq[i][j]=0;
+    								parq[i][j+1]=0;
+    								costCounter += costDouble;
+    							    }
+    							else {
+    							    parq[i][j]=0;
+    							    parq[i][j+1]=0;
+    							    costCounter += costDouble;
+    							}
+    						    else if (parq[i+1][j]==1)
+    							{
+    							    parq[i][j]=0;
+    							    parq[i+1][j]=0;
+    							    costCounter += costDouble;
+    							}
+    						//конец цикла проверки
+						}
+					    else if (j<parq[i].length)
+        						{
+        						    //основной цикл проверки для квадрата 2х2
+        						    if (parq[i][j]==1)
+        	    						    if (parq[i][j+1]==1)
+        	    							if (parq[i+1][j]==1)
+        	    							   {
+        	    								parq[i][j]=0;
+        	    								parq[i][j+1]=0;
+        	    								costCounter += costDouble;
+        	    							    }
+        	    							else {
+        	    							    parq[i][j]=0;
+        	    							    parq[i][j+1]=0;
+        	    							    costCounter += costDouble;
+        	    							}
+        	    						    else if (parq[i+1][j]==1)
+        	    							{
+        	    							    parq[i][j]=0;
+        	    							    parq[i+1][j]=0;
+        	    							    costCounter += costDouble;
+        	    							}
+        						    //конец цикла проверки для квадрата 2х2
+        						}
+					    	else {
+        					    	if (parq[i][j]==1 && parq[i+1][j]==1)
+            							{
+            							    parq[i][j]=0;
+            							    parq[i+1][j]=0;
+            							    costCounter += costDouble;
+            							}
+					    	}
+						
+							
+					    
+					    }
+					else {
+					    	if (parq[i][j]==1 && parq[i][j+1]==1)
+    							{
+    							    parq[i][j]=0;
+    							    parq[i+1][j]=0;
+    							    costCounter += costDouble;
+    							}
+				    	}
+    						    
+				} 
         		
-			
+			//алгоритм проверки на возможность починки паркета плиткой размером 1х1
+        				
 			
 			
 			
