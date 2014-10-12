694e502b77e830c4c28bdfc84a13dbf8893057ef
Den Corbie <corbie.den@mail.ru>
2014-10-12 16:45:45 +0400

    Прорабатывается ввод.

--- 2014.10.11 Yandex - Baseline predictors/src/ru/my1/corbie/Main.java
+++ 2014.10.11 Yandex - Baseline predictors/src/ru/my1/corbie/Main.java
@@ -14,7 +14,7 @@ public class Main
 		//предполагаем, что пользователь знает формат ввода данных
 		System.out.println("Введите данные: "); 
 		String a = sc.nextLine().trim() + " ";
-		List<Short> b = new ArrayList<Short>();
+		List<Integer> b = new ArrayList<Integer>();
 				
 		//обработка первой строки ввода
 		StringBuilder sb = new StringBuilder();
@@ -23,16 +23,70 @@ public class Main
 			if (Character.isDigit(a.charAt(i)))
 			    sb.append(a.charAt(i));
 			else {
-			    b.add(Short.parseShort(sb.toString()));
+			    b.add(Integer.parseInt(sb.toString()));
 			    sb.delete(0, sb.length());
 			    }
 			}
-		Short[] inner = b.toArray(new Short[4]);
-		short k = inner[0];
-		short U = inner[1];
-		short M = inner[2];
-		short D = inner[3];
-		short T = inner[4];
+		sb.delete(0, sb.length());
+		Integer[] inner = b.toArray(new Integer[5]);
+		b.clear();
+		int k = inner[0], U = inner[1], 
+			M = inner[2], D = inner[3], T = inner[4];
+		
+		//проверка правильно введённых данных
+		if (!(((1 < k) & (k < 101)) & ((0 < U) & (U < 10001)) &
+			((0 < M) & (M < 10001)) & ((0 < D) & (D < 100001)) & 
+			((0 < T) & (T < 1000001))))
+		    {
+			System.out.println("Введены некорректные данные");
+			return;
+		    }
+		
+		//имитация двумерного массива для рейтинга фильмов
+		int[][] ratingArray = new int[M][U];
+
+		//обработка последующих строк ввода			
+		for(short i=0; i< D; i++)
+			{
+			    String l = sc.nextLine().trim();
+			    for (byte j=0; j<l.length();j++)
+				{
+				    if (Character.isDigit(a.charAt(j)))
+					    sb.append(a.charAt(j));
+				    else {
+					b.add(Integer.parseInt(sb.toString()));
+					sb.delete(0, sb.length());
+					}
+				    Integer[] t = b.toArray(new Integer[3]);
+				    sb.delete(0, sb.length());
+				    b.clear();
+				    ratingArray[t[0]][t[1]] = t[2];
+				}
+			    }
+		for (int[] is : ratingArray)
+		    {
+			System.out.println(is);
+		    }
+		/*
+		for(short i=0; i< T; i++)
+			{
+			    String l = sc.nextLine().trim();
+			    for (byte j=0; j<l.length();j++)
+				{
+				    if (Character.isDigit(a.charAt(j)))
+					    sb.append(a.charAt(j));
+				    else {
+					b.add(Integer.parseInt(sb.toString()));
+					sb.delete(0, sb.length());
+					}
+				    Integer[] t = b.toArray(new Integer[2]);
+				    sb.delete(0, sb.length());
+				    b.clear();
+				}
+			    }
+		*/
 	    }
+	
+	
 
     }
